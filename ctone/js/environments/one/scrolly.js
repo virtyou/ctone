b10f722c0fc3e5997f1abb68f0677e9936a055d4
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-24 12:27:27 -0800

    scrolly environment w/ map and model

---
+++ ctone/js/environments/one/scrolly.js
@@ -0,0 +1,12 @@
+environments.one.scrolly = {
+	texture: "/maps/one/bgBITZs4a.jpg",
+	stripset: "/models/one/box.js",
+	repeat: [5, 6],
+	scale: [300, 300, 10000],
+	scroll: { speed: -0.05, repeat: true },
+	material: {
+		color: 0xffffff,
+		emissive: 0xffff00,
+		reflectivity: 0
+	}
+};
