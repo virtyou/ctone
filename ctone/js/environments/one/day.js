80ee0dc7c4363f8afac822f9192bf18aab3f1bd3
bubbleboy14 <mario.balibrera@gmail.com>
2020-12-04 00:12:56 -0800

    day/night environments

---
+++ ctone/js/environments/one/day.js
@@ -0,0 +1,5 @@
+environments.one.day = {
+	outside: "/maps/one/sky-day.jpg",
+	boxGeometry: [512, 512, 512],
+	material: { transparent: true, opacity: 0 }
+};
---
+++ ctone/js/environments/one/night.js
@@ -0,0 +1,5 @@
+environments.one.night = {
+	outside: "/maps/one/sky-night.png",
+	boxGeometry: [512, 512, 512],
+	material: { transparent: true, opacity: 0 }
+};
