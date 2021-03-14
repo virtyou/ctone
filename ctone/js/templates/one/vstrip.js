d1958e925d2e802e5fa0d77398260fd1050156a0
bubbleboy14 <mario.balibrera@gmail.com>
2021-03-13 20:45:54 -0800

    vstrip templates: flu, chickens, vr

---
+++ ctone/js/templates/one/vstrip.js
@@ -0,0 +1,20 @@
+templates.one.vstrip = {
+	flu: {
+		texture: "/maps/one/vstrip/flu.png",
+		width: 16384,
+		height: 320,
+		frames: 598
+	},
+	chickens: {
+		texture: "/maps/one/vstrip/chickens.png",
+		width: 16384,
+		height: 512,
+		frames: 1003
+	},
+	vr: {
+		texture: "/maps/one/vstrip/vr.png",
+		width: 16384,
+		height: 256,
+		frames: 502
+	}
+};
