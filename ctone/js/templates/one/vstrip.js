6829c814746275ee3b6c0096799e5a8f102431d6
bubbleboy14 <mario.balibrera@gmail.com>
2021-04-07 19:02:49 -0700

    added vstrip templates for flames, smoke, sparks, water

--- ctone/js/templates/one/vstrip.js
+++ ctone/js/templates/one/vstrip.js
@@ -10,5 +10,21 @@ templates.one.vstrip = {
 	vr: {
 		texture: "/maps/one/vstrip/vr.png",
 		frames: 502
-	}
+	},
+	flames: {
+		texture: "/maps/one/vstrip/env/flames.png",
+		frames: 300
+	},
+	smoke: {
+		texture: "/maps/one/vstrip/env/smoke.png",
+		frames: 1320
+	},
+	sparks: {
+		texture: "/maps/one/vstrip/env/sparks.png",
+		frames: 450
+	},
+	water: {
+		texture: "/maps/one/vstrip/env/water.png",
+		frames: 361
+	}	
 };
