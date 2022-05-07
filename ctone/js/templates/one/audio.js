b8b346f08cc3b828d0884655d0cecdb56f5f10c7
bubbleboy14 <mario.balibrera@gmail.com>
2022-05-06 17:23:26 -0700

    audio refactor supporting wet collection

--- ctone/js/templates/one/audio.js
+++ ctone/js/templates/one/audio.js
@@ -1,30 +1,60 @@
 templates.one.audio = {};
-var fb = {
-	eel: "splash",
-	fish: "splash",
-	ant: "creep",
-	spider: "creep",
-	centipede: "creep",
-	bird: 3,
-	bee: 1,
-	wasp: 1,
-	cat: 5,
-	chicken: 1,
-	cow: 3,
-	dog: 3,
-	horse: 4,
-	pig: 6,
-	sheep: 4
+
+var audioMap = templates.one.audio.amap = {
+	wet: {
+		within: "within",
+		without: "without",
+		inout: "inout",
+		splash: 2
+	},
+	fauna: {
+		eel: {
+			number: 2,
+			name: "splash",
+			collection: "wet"
+		},
+		fish: {
+			number: 2,
+			name: "splash",
+			collection: "wet"
+		},
+		ant: "creep",
+		spider: "creep",
+		centipede: "creep",
+		bird: 3,
+		bee: 1,
+		wasp: 1,
+		cat: 5,
+		chicken: 1,
+		cow: 3,
+		dog: 3,
+		horse: 4,
+		pig: 6,
+		sheep: 4
+	}
 };
-var toaf = templates.one.audio.fauna = zero.core.Fauna.audio = {};
-var setAud = function(f) {
-	var aset = toaf[f] = [];
-	var v = fb[f];
-	if (isNaN(v))
-		aset.push("/audio/one/fauna/" + v + ".mp3");
+
+var assets = templates.one.audio.assets = { wet: {} };
+assets.fauna = zero.core.Fauna.audio = {};
+
+var audSet = function(collection, name, number, aset) {
+	for (var i = 1; i <= number; i++)
+		aset.push("/audio/one/" + collection + "/" + name + "/" + i + ".mp3");
+};
+
+var setAud = function(collection, sound) {
+	var aset = assets[collection][sound] = [],
+		v = audioMap[collection][sound];
+	if (typeof v == "string")
+		aset.push("/audio/one/" + collection + "/" + v + ".mp3");
+	else if (!isNaN(v))
+		audSet(collection, sound, v, aset);
+	else if (v.number)
+		audSet(v.collection, v.name, v.number, aset);
 	else
-		for (var i = 1; i <= v; i++)
-			aset.push("/audio/one/fauna/" + f + "/" + i + ".mp3");
+		aset.push("/audio/one/" + v.collection + "/" + v.name + ".mp3");
 };
-for (var f in fb)
-	setAud(f);
+
+for (var collection in audioMap)
+	for (var sound in audioMap[collection])
+		setAud(collection, sound);
