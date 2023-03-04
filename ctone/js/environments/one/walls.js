d2c3da5c364474b1b62484dc58c299673c05e89c
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-03 22:33:54 -0800

    ramps

--- ctone/js/environments/one/walls.js
+++ ctone/js/environments/one/walls.js
@@ -3,20 +3,45 @@ environments.one.walls = {
 	boxGeometry: [6400, 6400, 6400],
 	material: { side: THREE.BackSide },
 	ramp: {
-		
+		texture: "/maps/one/stonestairs.png",
+		parts: [{
+			planeGeometry: true,
+			position: [2800, 1200, 0],
+			rotation: [-2, 0, 0],
+			scale: [8, 200, 1],
+			material: {
+				transparent: true
+			}
+		}, {
+			planeGeometry: true,
+			position: [-2800, -400, 0],
+			rotation: [2, 0, 0],
+			scale: [8, 200, 1],
+			material: {
+				transparent: true
+			}
+		}, {
+			planeGeometry: true,
+			position: [2800, -2000, 0],
+			rotation: [-2, 0, 0],
+			scale: [8, 200, 1],
+			material: {
+				transparent: true
+			}
+		}]
 	},
 	obstacle: {
 		texture: "/maps/one/stonewall.jpg",
 		parts: [{
 			dimensions: [4800, 6400, 4800]
 		}, {
-			position: [-400, 4800, 0],
-			dimensions: [5600, 800, 6400]
-		}, {
-			position: [400, 3200, 0],
-			dimensions: [5600, 800, 6400]
-		}, {
 			position: [-400, 1600, 0],
+			dimensions: [5600, 800, 6400]
+		}, {
+			position: [400, 0, 0],
+			dimensions: [5600, 800, 6400]
+		}, {
+			position: [-400, -1600, 0],
 			dimensions: [5600, 800, 6400]
 		}]
 	}
