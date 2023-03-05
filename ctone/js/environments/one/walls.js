23796db0c6e78218fc1913cc8def66cd60765755
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-05 06:55:51 -0800

    walls environment tweakz

--- ctone/js/environments/one/walls.js
+++ ctone/js/environments/one/walls.js
@@ -1,33 +1,29 @@
 environments.one.walls = {
 	matcat: "Phong",
 	boxGeometry: [6400, 6400, 6400],
+	texture: "/maps/one/stonewall.jpg",
 	material: { side: THREE.BackSide },
 	ramp: {
 		texture: "/maps/one/stonestairs.png",
+		material: {
+			transparent: true,
+			side: THREE.DoubleSide
+		},
 		parts: [{
 			planeGeometry: true,
 			position: [3000, 1200, 0],
-			rotation: [2, 0, 0],
-			scale: [4, 200, 1],
-			material: {
-				transparent: true
-			}
+			rotation: [1.815, 0, 0],
+			scale: [4, 70, 1]
 		}, {
 			planeGeometry: true,
 			position: [-3000, -400, 0],
-			rotation: [-2, 0, 0],
-			scale: [4, 200, 1],
-			material: {
-				transparent: true
-			}
+			rotation: [-1.815, 0, 0],
+			scale: [4, 70, 1]
 		}, {
 			planeGeometry: true,
-			position: [3000, -2000, 0],
-			rotation: [-2, 0, 0],
-			scale: [4, 200, 1],
-			material: {
-				transparent: true
-			}
+			position: [-3000, -2000, 0],
+			rotation: [-1.815, 0, 0],
+			scale: [4, 70, 1]
 		}]
 	},
 	obstacle: {
@@ -36,22 +32,22 @@ environments.one.walls = {
 			position: [0, 400, 0],
 			dimensions: [4800, 5600, 4800]
 		}, {
-			position: [-400, 1600, 0],
-			dimensions: [5600, 800, 6400]
+			position: [-200, 1600, 0],
+			dimensions: [6000, 800, 6400]
 		}, {
-			position: [400, 0, 0],
-			dimensions: [5600, 800, 6400]
+			position: [200, 0, 0],
+			dimensions: [6000, 800, 6400]
 		}, {
-			position: [400, -1600, 0],
-			dimensions: [5600, 800, 6400]
+			position: [200, -1600, 0],
+			dimensions: [6000, 800, 6400]
 		}, {
-			position: [2800, -1500, 0],
-			dimensions: [800, 3400, 1600]
+			position: [2800, -1300, 0],
+			dimensions: [800, 3800, 1600]
 		}, {
-			position: [-2400, -1000, 2400],
+			position: [-2200, -1000, 2200],
 			dimensions: [800, 4400, 800]
 		}, {
-			position: [-2400, -1000, -2400],
+			position: [-2200, -1000, -2200],
 			dimensions: [800, 4400, 800]
 		}]
 	}
