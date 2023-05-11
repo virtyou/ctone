1d2a705aab519ab85e03d90d4060c5e7957f53a9
bubbleboy14 <mario.balibrera@gmail.com>
2023-05-10 17:09:02 -0700

    peak environment

---
+++ ctone/js/environments/one/peak.js
@@ -0,0 +1,11 @@
+environments.one.peak = {
+	outside: "/maps/one/sky-day.jpg",
+	boxGeometry: [1024, 1024, 1024],
+	obstacle: {
+		texture: "/maps/one/leaves.jpg",
+		parts: [{
+			position: [0, -1024, 0],
+			dimensions: [1024, 1024, 1024]
+		}]
+	}
+};
