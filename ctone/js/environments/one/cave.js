d2ee3d80a665522eb2396ce13e5aa871170664de
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-16 22:40:59 -0700

    cave: adjusted boulder levels

--- ctone/js/environments/one/cave.js
+++ ctone/js/environments/one/cave.js
@@ -11,27 +11,27 @@ environments.one.cave = {
 			sphereGeometry: bsize,
 			sphereSegs: 5,          // 3-8     ; default 5
 			geoThetaLength: Math.PI, // 0.1-2PI ; default PI
-			position: [1000, -1000, 1000]
+			position: [1000, -1100, 1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 3,
 			geoThetaLength: Math.PI,
-			position: [-1000, -1000, 1000]
+			position: [-1000, -1100, 1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 8,
 			geoThetaLength: Math.PI,
-			position: [1000, -1000, -1000]
+			position: [1000, -1100, -1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 5,
-			geoThetaLength: 0.1,
-			position: [-1000, -1000, -1000]
+			geoThetaLength: 1,
+			position: [-1000, -1200, -1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 5,
 			geoThetaLength: Math.PI * 2,
-			position: [0, -1000, 0]
+			position: [0, -1100, 0]
 		}]
 	}
 };
