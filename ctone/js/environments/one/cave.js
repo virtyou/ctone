f68fcf58801b8193989e3be22fbda45f587b7424
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-19 12:55:16 -0700

    cave environment: stalactites and stalagmites

--- ctone/js/environments/one/cave.js
+++ ctone/js/environments/one/cave.js
@@ -1,26 +1,56 @@
-var bsize = 100;
+var bsize = 100, P = Math.PI;
 
 environments.one.cave = {
+	texture: "/maps/one/rock3.jpg",
 	shell: {
-		texture: "/maps/one/rock3.jpg",
 		dimensions: [2400, 2400, 2400],
 	},
+	stala: {
+		parts: [{
+			coneGeometry: 100,
+			position: [500, -1100, 500]
+		}, {
+			coneGeometry: 100,
+			position: [-500, -1100, 500]
+		}, {
+			coneGeometry: 100,
+			position: [500, -1100, -500]
+		}, {
+			coneGeometry: 100,
+			position: [-500, -1100, -500]
+		}, {
+			coneGeometry: 100,
+			rotation: [P, 0, 0],
+			position: [500, 1100, 500]
+		}, {
+			coneGeometry: 100,
+			rotation: [P, 0, 0],
+			position: [-500, 1100, 500]
+		}, {
+			coneGeometry: 100,
+			rotation: [P, 0, 0],
+			position: [500, 1100, -500]
+		}, {
+			coneGeometry: 100,
+			rotation: [P, 0, 0],
+			position: [-500, 1100, -500]
+		}]
+	},
 	boulder: {
-		texture: "/maps/one/rock3.jpg",
 		parts: [{
 			sphereGeometry: bsize,
 			sphereSegs: 5,          // 3-8     ; default 5
-			geoThetaLength: Math.PI, // 0.1-2PI ; default PI
+			geoThetaLength: P, // 0.1-2PI ; default PI
 			position: [1000, -1100, 1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 3,
-			geoThetaLength: Math.PI,
+			geoThetaLength: P,
 			position: [-1000, -1100, 1000]
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 8,
-			geoThetaLength: Math.PI,
+			geoThetaLength: P,
 			position: [1000, -1100, -1000]
 		}, {
 			sphereGeometry: bsize,
@@ -30,7 +60,7 @@ environments.one.cave = {
 		}, {
 			sphereGeometry: bsize,
 			sphereSegs: 5,
-			geoThetaLength: Math.PI * 2,
+			geoThetaLength: P * 2,
 			position: [0, -1100, 0]
 		}]
 	}
