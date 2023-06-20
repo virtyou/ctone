f4e3f37027580e35260e821ce984679c3510ea9e
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-20 16:42:40 -0700

    peak boulder{}s

--- ctone/js/environments/one/peak.js
+++ ctone/js/environments/one/peak.js
@@ -7,5 +7,20 @@ environments.one.peak = {
 			position: [0, -1024, 0],
 			dimensions: [1024, 1024, 1024]
 		}]
+	},
+	boulder: {
+		texture: "/maps/one/rock3.jpg",
+		parts: [{
+			sphereSegs: 5,
+			sphereGeometry: 1024,
+			position: [1300, -200, 0],
+			rotation: [0, Math.PI, 0]
+		}, {
+			brittle: true,
+			sphereSegs: 4,
+			sphereGeometry: 100,
+			position: [450, -450, 450],
+			rotation: [0, Math.PI / 4, 0]
+		}]
 	}
 };
