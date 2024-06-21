43f537f251ca38474f1792bfe235791dfd9d6148
bubbleboy14 <mario.balibrera@gmail.com>
2024-06-21 00:37:35 -0700

    fbx models (dragon/guy) and templates

---
+++ ctone/js/templates/one/fbx.js
@@ -0,0 +1,10 @@
+templates.one.fbx = {
+	dragon: { // source: see readme.txt
+		loader: "FBXLoader",
+		stripset: "/models/one/fbx/dragon/dragon.fbx"
+	},
+	guy: { // source: https://gist.github.com/bellbind/f78746c37f31742596cc2f67326ad595
+		loader: "FBXLoader",
+		stripset: "/models/one/fbx/guy/guy.fbx"
+	}
+};
