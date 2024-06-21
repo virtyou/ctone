97ff4a9352a2c3297ba8aff9d81f25523c3f2bf2
bubbleboy14 <mario.balibrera@gmail.com>
2024-06-21 01:42:17 -0700

    tweaked scales

--- ctone/js/templates/one/fbx.js
+++ ctone/js/templates/one/fbx.js
@@ -1,12 +1,12 @@
 var zbf = zero.base.fauna, ftz = templates.one.fbx = {
 	dragon: { // source: see readme.txt
 		loader: "FBXLoader",
-		scale: [0.08, 0.08, 0.08],
+		scale: [0.04, 0.04, 0.04],
 		stripset: "/models/one/fbx/dragon/dragon.fbx"
 	},
 	guy: { // source: https://gist.github.com/bellbind/f78746c37f31742596cc2f67326ad595
 		loader: "FBXLoader",
-		scale: [4, 4, 4],
+		scale: [2, 2, 2],
 		stripset: "/models/one/fbx/guy/guy.fbx"
 	}
 };
