a41a075353b6502dc3f603304a94740af0b682f6
bubbleboy14 <mario.balibrera@gmail.com>
2024-07-10 16:05:23 -0700

    fbx template credits

--- ctone/js/templates/one/fbx.js
+++ ctone/js/templates/one/fbx.js
@@ -1,24 +1,27 @@
 var m, zbf = zero.base.fauna, zcf = zero.core.Fauna, ftz = templates.one.fbx = {
-	dragon: { // source: https://free3d.com/3d-model/black-dragon-rigged-and-game-ready-92023.html
+	dragon: {
 		scale: [0.04, 0.04, 0.04],
 		stripset: "/models/one/fbx/dragon/Dragon_Baked_Actions_fbx_7.4_binary.fbx",
+		credit: "https://free3d.com/3d-model/black-dragon-rigged-and-game-ready-92023.html",
 		anims: {
 			walk: 0,
 			attack: 1,
 			hurt: 2
 		}
 	},
-	guy: { // source: https://gist.github.com/bellbind/f78746c37f31742596cc2f67326ad595
+	guy: {
 		scale: [2, 2, 2],
-		stripset: "/models/one/fbx/guy/guy.fbx"
+		stripset: "/models/one/fbx/guy/guy.fbx",
+		credit: "https://gist.github.com/bellbind/f78746c37f31742596cc2f67326ad595"
 	},
-	wolf: { // source: https://free3d.com/3d-model/wolf-rigged-and-game-ready-42808.html
+	wolf: {
 		speed: 60,
 		backwards: true,
 		scale: [1, -1, 1],
-		stripset: "/models/one/fbx/wolf/Wolf.fbx"
+		stripset: "/models/one/fbx/wolf/Wolf.fbx",
+		credit: "https://free3d.com/3d-model/wolf-rigged-and-game-ready-42808.html"
 	},
-	arachnid: { // source: https://free3d.com/3d-model/spider-animated-low-poly-and-game-ready-87147.html
+	arachnid: {
 		anims: {
 			walk: 0,
 			hurt: 1,
@@ -28,13 +31,15 @@ var m, zbf = zero.base.fauna, zcf = zero.core.Fauna, ftz = templates.one.fbx = {
 		timeScale: -1,
 		backwards: true,
 		stripset: "/models/one/fbx/arachnid/Spider.fbx",
+		credit: "https://free3d.com/3d-model/spider-animated-low-poly-and-game-ready-87147.html",
 		onbuild: function(arachnid) {
 			arachnid.thring.remove(arachnid.thring.children[1]);
 		}
 	},
 	castle: {
 		scale: [80, 80, 80],
-		stripset: "/models/one/fbx/castle/castle.fbx"
+		stripset: "/models/one/fbx/castle/castle.fbx",
+		credit: "https://free3d.com/3d-model/fantasy-castle-40715.html"
 	}
 };
 for (m in ftz)
