a7605cf1434b39a9ab9a7baf78b241e009f7d6d9
bubbleboy14 <mario.balibrera@gmail.com>
2024-08-09 19:26:41 -0700

    tombs - basic bones

---
+++ ctone/js/environments/one/tombs.js
@@ -0,0 +1,36 @@
+environments.one.tombs = {
+	texture: "/maps/one/rock3.jpg",
+	shell: {
+		dimensions: [2000, 1200, 2000]
+	},
+	obstacle: {
+		texture: "/maps/one/stonewall.jpg",
+		parts: [{
+			position: [-500, 0, 750],
+			dimensions: [1000, 400, 500]
+		}, {
+			position: [-500, 0, -750],
+			dimensions: [1000, 300, 500]
+		}, {
+			position: [500, 0, -800],
+			dimensions: [1000, 600, 400]
+		}]
+	},
+	ramp: {
+		texture: "/maps/one/stonewall.jpg",
+		material: {
+			side: THREE.DoubleSide
+		},
+		parts: [{
+			planeGeometry: true,
+			scale: [2, 16, 1],
+			rotation: [-2.1, 0, 0],
+			position: [-900, -200, -190]
+		}, {
+			planeGeometry: true,
+			scale: [2, 16, 1],
+			rotation: [2.15, 0, 0],
+			position: [900, -150, 70]
+		}]
+	}
+};
