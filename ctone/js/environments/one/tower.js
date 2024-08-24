cd7d417ce35312fb0803684124cfec941c420092
bubbleboy14 <mario.balibrera@gmail.com>
2024-08-23 23:51:14 -0700

    tower: squatter; backshifted mid height obstacle

--- ctone/js/environments/one/tower.js
+++ ctone/js/environments/one/tower.js
@@ -1,24 +1,24 @@
 environments.one.tower = {
 	outside: "/maps/one/sky-day.jpg",
-	boxGeometry: [8000, 4000, 8000],
+	boxGeometry: [8000, 2000, 8000],
 	obstacle: {
 		texture: "/maps/one/leaves.jpg",
 		parts: [{
-			position: [0, -3600, -2000],
-			dimensions: [8000, 800, 4000]
+			position: [0, -1800, -2000],
+			dimensions: [8000, 400, 4000]
 		}, {
-			position: [0, -2800, -2000],
-			dimensions: [4000, 800, 2000]
+			position: [0, -1400, -2400],
+			dimensions: [4000, 400, 2000]
 		}, {
-			position: [0, -2500, 0],
-			dimensions: [2000, 3000, 2000]
+			position: [0, -1250, 0],
+			dimensions: [2000, 1500, 2000]
 		}]
 	},
 	floor: {
 		texture: "/maps/one/leaves.jpg",
 		parts: [{
 			planeGeometry: true,
-			position: [0, -4000, 2000],
+			position: [0, -2000, 2000],
 			scale: [80, 40, 1],
 			material: {
 				side: THREE.BackSide
