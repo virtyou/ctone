e3fe95ccad5454fddc2207d71c3d4614e44f9694
bubbleboy14 <mario.balibrera@gmail.com>
2024-11-20 15:07:28 -0800

    appliance template with elevator{}

---
+++ ctone/js/templates/one/appliance.js
@@ -0,0 +1,12 @@
+templates.one.appliance = {
+	elevator: {
+		walltex: "/maps/one/bluefab.jpg",
+		floortex: "/maps/one/bunny_teeth.jpg",
+		door: {
+			texture: "/maps/one/edoor.png",
+			material: {
+				transparent: true
+			}
+		}
+	}
+};
