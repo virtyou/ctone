9ba421e892f1d7133fbb1cd30b1f67f0c16ff359
bubbleboy14 <mario.balibrera@gmail.com>
2024-11-22 23:09:03 -0800

    apartment appliances{} reorg

--- ctone/js/environments/one/apartment.js
+++ ctone/js/environments/one/apartment.js
@@ -9,47 +9,46 @@ environments.one.apartment = {
 				building: {}
 			}
 		},
-		appliances: [{
-			thing: "Panel",
-			name: "panel0",
-			lever: [{ circuit: "building" }],
-			position: [0, -150, 400],
-			rotation: [0, Math.PI, 0]
-		}, {
-			name: "gate0",
-			appliance: "Gate",
-			width: 120,
-			height: 160,
-			position: [0, -170, 300],
-			rotation: [0, Math.PI, 0],
-			door: {
-				texture: "/maps/one/metaldoor.png"
+		appliances: {
+			panel: {
+				parts: [{
+					position: [0, -150, 400],
+					rotation: [0, Math.PI, 0],
+					lever: [{ circuit: "building" }]
+				}]
+			},
+			gate: {
+				parts: [{
+					width: 120,
+					height: 160,
+					position: [0, -170, 300],
+					rotation: [0, Math.PI, 0],
+					door: {
+						texture: "/maps/one/metaldoor.png"
+					}
+				}]
+			},
+			elevator: {
+				parts: [{
+					circuit: "building",
+					position: [0, 0, -340],
+					template: "templates.one.appliance.elevator"
+				}]
+			},
+			bulb: {
+				circuit: "building",
+				parts: [{
+					position: [0, 245, 50],
+					rotation: [Math.PI, 0, 0]
+				}, {
+					position: [0, 75, 50],
+					rotation: [Math.PI, 0, 0]
+				}, {
+					position: [0, -95, 50],
+					rotation: [Math.PI, 0, 0]
+				}]
 			}
-		}, {
-			name: "elevator0",
-			appliance: "Elevator",
-			circuit: "building",
-			template: "templates.one.appliance.elevator",
-			position: [0, 0, -340]
-		}, {
-			name: "bulb0",
-			appliance: "Bulb",
-			circuit: "building",
-			position: [0, 245, 50],
-			rotation: [Math.PI, 0, 0]
-		}, {
-			name: "bulb1",
-			appliance: "Bulb",
-			circuit: "building",
-			position: [0, 75, 50],
-			rotation: [Math.PI, 0, 0]
-		}, {
-			name: "bulb2",
-			appliance: "Bulb",
-			circuit: "building",
-			position: [0, -95, 50],
-			rotation: [Math.PI, 0, 0]
-		}]
+		}
 	},
 	obstacle: {
 		texture: "/maps/one/rug4.jpg",
