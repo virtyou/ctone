92cc3d7692798f20373f0be4a40e5789015dfbbe
bubbleboy14 <mario.balibrera@gmail.com>
2024-12-03 09:36:00 -0800

    appliance templates: moved elevator{} to elevator.spooky{}; gate{} - metal and chain

--- ctone/js/templates/one/appliance.js
+++ ctone/js/templates/one/appliance.js
@@ -1,19 +1,36 @@
 templates.one.appliance = {
 	elevator: {
-		walltex: "/maps/one/bluefab.jpg",
-		floortex: "/maps/one/bunny_teeth.jpg",
-		light: {
-			invariance: 5,
-			intensity: 0.4
-		},
-		door: {
-			texture: "/maps/one/edoor.png",
-			material: {
-				transparent: true
+		spooky: {
+			walltex: "/maps/one/bluefab.jpg",
+			floortex: "/maps/one/bunny_teeth.jpg",
+			light: {
+				invariance: 5,
+				intensity: 0.4
+			},
+			door: {
+				texture: "/maps/one/edoor.png",
+				material: {
+					transparent: true
+				}
+			},
+			floordoor: {
+				texture: "/maps/one/rusty.jpg"
+			}
+		}
+	},
+	gate: {
+		metal: {
+			door: {
+				texture: "/maps/one/metaldoor.png"
 			}
 		},
-		floordoor: {
-			texture: "/maps/one/rusty.jpg"
+		chain: {
+			door: {
+				texture: "/maps/one/chainlink4.png",
+				material: {
+					transparent: true
+				}
+			}
 		}
 	}
 };
