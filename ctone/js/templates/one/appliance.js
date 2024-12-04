b7c213e5f0606882e227b3eaa0e1efb857f3e6eb
bubbleboy14 <mario.balibrera@gmail.com>
2024-12-03 22:38:35 -0800

    computweax

--- ctone/js/templates/one/appliance.js
+++ ctone/js/templates/one/appliance.js
@@ -35,6 +35,7 @@ templates.one.appliance = {
 	},
 	computer: {
 		tablet: {
+			keyboard: false,
 			parts: [{
 				name: "base",
 				position: [0, 0, -1],
@@ -43,10 +44,11 @@ templates.one.appliance = {
 		},
 		lcd: {
 			screenPos: [0, 15, 12],
-			screenDims: [18, 18],
+			screenDims: [22, 16],
 			parts: [{
 				name: "base",
-				halfSphere: 8
+				halfSphere: 8,
+				position: [0, -1, 0]
 			}, {
 				name: "stem",
 				coneGeometry: true,
@@ -64,25 +66,26 @@ templates.one.appliance = {
 				scale: [10, 5, 2]
 			}, {
 				name: "monitor",
-				boxGeometry: [20, 20, 3],
+				boxGeometry: [24, 18, 3],
 				position: [0, 15, 10]
 			}]
 		},
 		crt: {
-			screenPos: [0, 15, 11],
+			screenPos: [0, 19, 11],
 			screenDims: [15, 15],
 			parts: [{
 				name: "base",
+				position: [0, 4, 0],
 				boxGeometry: [30, 10, 30]
 			}, {
 				name: "back",
 				halfSphere: 8,
-				position: [0, 15, 0],
+				position: [0, 19, 0],
 				rotation: [-Math.PI / 2, 0, 0]
 			}, {
 				name: "monitor",
-				boxGeometry: [20, 20, 12],
-				position: [0, 15, 5]
+				position: [0, 19, 5],
+				boxGeometry: [20, 20, 12]
 			}]
 		}
 	}
