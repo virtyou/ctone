89b3724df984b35860c5d50eef59654e4c3ef53c
bubbleboy14 <mario.balibrera@gmail.com>
2024-12-12 15:38:20 -0800

    environments: renamed apartment->backhalls; new apartment (1st pass) w/ 2 floors (and side halls), basement, elevators

--- ctone/js/environments/one/apartment.js
+++ ctone/js/environments/one/apartment.js
@@ -2,7 +2,7 @@ environments.one.apartment = {
 	texture: "/maps/one/yellowcloth.jpg",
 	shadows: true,
 	shell: {
-		dimensions: [120, 500, 800]
+		dimensions: [500, 500, 500]
 	},
 	electrical: {
 		circuits: {
@@ -11,72 +11,44 @@ environments.one.apartment = {
 			}
 		},
 		appliances: {
-			panel: {
-				parts: [{
-					position: [0, -150, 400],
-					rotation: [0, Math.PI, 0],
-					switch: [{ circuit: "bulb0" }, { circuit: "bulb1" }, { circuit: "bulb2" }]
-				}, {
-					position: [60, -170, -200],
-					rotation: [0, -Math.PI / 2, 0],
-					button: [{ appliance: "elevator0", order: "bottom"}]
-				}, {
-					position: [60, 0, -200],
-					rotation: [0, -Math.PI / 2, 0],
-					button: [{ appliance: "elevator0", order: "obstacle0"}]
-				}, {
-					position: [60, 170, -200],
-					rotation: [0, -Math.PI / 2, 0],
-					button: [{ appliance: "elevator0", order: "obstacle1"}]
-				}]
-			},
-			gate: {
-				parts: [{
-					width: 120,
-					height: 160,
-					position: [0, -170, 300],
-					rotation: [0, Math.PI, 0],
-					template: "templates.one.appliance.gate.metal"
-				}]
-			},
 			elevator: {
 				parts: [{
 					circuit: "building",
-					position: [0, 0, -340],
+					position: [0, 0, 0],
 					template: "templates.one.appliance.elevator.spooky"
-				}]
-			},
-			bulb: {
-				circuit: "building",
-				parts: [{
-					invariance: 6,
-					intensity: 0.6,
-					position: [0, 245, 50],
-					rotation: [Math.PI, 0, 0]
-				}, {
-					flickRate: 3,
-					invariance: 4,
-					intensity: 0.6,
-					position: [0, 75, 50],
-					rotation: [Math.PI, 0, 0]
-				}, {
-					flickRate: 2,
-					invariance: 2,
-					intensity: 0.2,
-					position: [0, -95, 50],
-					rotation: [Math.PI, 0, 0]
 				}]
 			}
 		}
 	},
+	floor: {
+		texture: "/maps/one/rug4.jpg",
+		material: {
+			side: THREE.DoubleSide
+		},
+		parts: [{
+			planeGeometry: true,
+			position: [0, -85, 400],
+			scale: [5, 2, 1]
+		}, {
+			planeGeometry: true,
+			position: [0, 85, 400],
+			scale: [5, 2, 1]
+		}]
+	},
 	obstacle: {
 		texture: "/maps/one/rug4.jpg",
 		parts: [{
-			position: [0, -85, 60],
-			dimensions: [120, 10, 680]
+			position: [-200, -85, -100],
+			dimensions: [100, 10, 300]
 		}, {
-			position: [0, 85, 60],
-			dimensions: [120, 10, 680]
+			position: [200, -85, -100],
+			dimensions: [100, 10, 300]
+		}, {
+			position: [-200, 85, -100],
+			dimensions: [100, 10, 300]
+		}, {
+			position: [200, 85, -100],
+			dimensions: [100, 10, 300]
 		}]
 	}
 };
